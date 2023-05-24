d040a47b6821de27b4cb938a6e49170b33c91430
Jorge <jorge>
2023-05-24 17:03:23 +0800

    Initial commit

---
+++ src/main/java/com/bibvip/demo/model/User.java
@@ -0,0 +1,20 @@
+package com.bibvip.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
